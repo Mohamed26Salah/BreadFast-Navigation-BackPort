c86eafe988253501a39c8ea86d738b63da300ec6
Mohamed26Salah <121755841+Mohamed27Salah@users.noreply.github.com>
2025-02-09 12:05:39 +0200

    add: create project

---
+++ BreadFast-New-Navigation/BreadFast-New-Navigation/BreadFast_New_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreadFast_New_NavigationApp.swift
+//  BreadFast-New-Navigation
+//
+//  Created by Mohamed Salah on 09/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct BreadFast_New_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
